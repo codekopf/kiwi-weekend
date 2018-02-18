3330efff5b47af5a7569832d5d3206ba4707aea1
Andrej Buday <andrej.buday@gmail.com>
2018-02-18 11:31:59 +0100

    Improvements 2018

--- book_flight.py
+++ book_flight.py
@@ -1,10 +1,16 @@
 import sys
 import json
-import urllib.request
 import argparse
+import urllib.request
+import urllib.parse
+
 import requests
 
+#from urllib.request import urlopen, urlparse
+#import requests
+
 from checks import *
+
 
 def main(argv):
 
@@ -44,74 +50,113 @@ def main(argv):
 
     args = parser.parse_args()
 
-    ### Set dates
+    # Set dates
     validate_date(args.date)
     dateFrom = datetime.datetime.strptime(args.date, "%Y-%m-%d")
 
-    ### If return, set date for return flight
+    # If return, set date for return flight
     if args.ret is not None:
         dayRange = int(args.ret)
         dateTo = dateFrom + datetime.timedelta(days=dayRange)
         print(dateFrom)
         print(dateTo)
 
-        ### TODO ambiguous condition
+        # TODO ambiguous condition
         if dateFrom >= dateTo:
             print("Start date is lower or equal to return date")
             os._exit()
-        ### TODO Check if search date is not less than current date
+        # TODO Check if search date is not less than current date
 
-    ## Set search conditions - cheapest option as default
+    # Set search conditions - cheapest option as default
     if args.cheapest is False and args.shortest is True:
         option = "duration"
     else:
         option = "price"
 
-    ### Set and check airports
-    ### TODO ambiguous information
+    # Set and check airports
+    # TODO ambiguous information
     airport_name_departure = check_airport_ITA_code(args.airportFrom)
     airport_name_arrival = check_airport_ITA_code(args.airportTo)
 
-    ### CORE ###
+    # CORE #
     if args.ret is not None:
-        flight = "Return"   ## TODO
-        url = URL_builder(args.airportFrom, args.airportTo, dateFrom, dateTo=dateTo, option=option)
+        flight = "Return"   # TODO
+        url = url_builder(args.airportFrom, args.airportTo, dateFrom, dateTo=dateTo, option=option)
         print("HALOOOO")
     else:
-        flight = "One way"   ## TODO
-        url = URL_builder(args.airportFrom, args.airportTo, dateFrom, option=option)
+        flight = "One way"   # TODO
+        url = url_builder(args.airportFrom, args.airportTo, dateFrom, option=option)
 
 
     print(url)
 
     # TODO Change name URL_laoder
-    booking_token = URL_loader(url)
+    url_data = return_url_content(url)
+    url_data_json = json.loads(url_data)
 
-    ### TODO Mockup object
-    #payload = { 'booking_token': booking_token, 'currency': "EUR", 'passengers': [{ 'lastName': "tomson", 'title': "Mr", "firstName": "isabel", "email": "asd@asd.com", "documentID": "4888789", "birthday": "1920-12-30"}]}
-    #""" % (booking_token)
+    if len(url_data_json['data']) != 0:
+        price = url_data_json['data'][0]['price']
+        print(price)
+    extracted_booking_token = url_data_json['data'][0]['booking_token']
 
-    payload = {
-        "booking_token": "GamFTRHoBQ/v6QB8Irykhs320XfjZPfZzlYyhc7jew1a69GAWqezecCYt+cWhnpzWuptXhyCw2ZUzQhvPHnlJy3io/iLupqyLALq00u+jzH66tfBzdZrHNhDeb2nTRPr5Hvey4zRvvRFSPAbJvkrRO+nEw0Mtde3RkitFiK08ayXWfm+icsCSKyBN+rRSsf+2Ot/G/XytVGJyu5cqXq0f9F6B9GQjn9TMiCCvqgvFt4q1yc/bdf84cBao/RwWU7o9ufC/P5EelqxUm2d2v+7HJyfYBVjvnd68qWwRTh/E18uPVBljGr1VNuhCnPajs6SW455ydpqU8LzVJov8fr4rYykcO0JmmSNgK6znDG6S7Ik4vkP4YxOJjQWs7zWjknrdBbF5y5D5R/PkYSBro6MbD/d/9mOxTyKYRHvba49PIqwoNmZ0I3xifL1BolMU9UropYVPAqaZ7w/GpwVqCGyFw==",
-        "currency": "EUR",
-        "passengers": [
-            {
-                "lastName": "tomson",
-                "title": "Mr",
-                "firstName": "isabel",
-                "email": "asd@asd.com",
-                "documentID": "4888789",
-                "birthday": "1920-12-30"
-            }
-        ]
-    }
 
-    url = 'http://37.139.6.125:8080/booking'
-    headers = {'content-type': 'application/json'}
 
-    r = requests.post(url, data = json.dumps(payload), headers=headers)
-    x = json.loads(r.text)
-    print(x['pnr'])
+    # TODO Mockup object
+    json_data = {}
+    json_data['booking_token'] = extracted_booking_token
+    json_data['bags'] = "0"
+    json_data['currency'] = "EUR"
+    json_data['passengers'] = [{'lastName': "tomson", 'title': "Mr", "firstName": "isabel", "email": "asd@asd.com", "documentID": "4888789", "birthday": "1920-12-30"}]
+
+    json_data = json.dumps(json_data)
+
+    #payload = { booking_token': extracted_booking_token, 'currency': "EUR", 'passengers': [{ 'lastName': "tomson", 'title': "Mr", "firstName": "isabel", "email": "asd@asd.com", "documentID": "4888789", "birthday": "1920-12-30"}]}
+    # """ % (booking_token)
+
+    # payload = {
+    #     "booking_token": "GamFTRHoBQ/v6QB8Irykhs320XfjZPfZzlYyhc7jew1a69GAWqezecCYt+cWhnpzWuptXhyCw2ZUzQhvPHnlJy3io/iLupqyLALq00u+jzH66tfBzdZrHNhDeb2nTRPr5Hvey4zRvvRFSPAbJvkrRO+nEw0Mtde3RkitFiK08ayXWfm+icsCSKyBN+rRSsf+2Ot/G/XytVGJyu5cqXq0f9F6B9GQjn9TMiCCvqgvFt4q1yc/bdf84cBao/RwWU7o9ufC/P5EelqxUm2d2v+7HJyfYBVjvnd68qWwRTh/E18uPVBljGr1VNuhCnPajs6SW455ydpqU8LzVJov8fr4rYykcO0JmmSNgK6znDG6S7Ik4vkP4YxOJjQWs7zWjknrdBbF5y5D5R/PkYSBro6MbD/d/9mOxTyKYRHvba49PIqwoNmZ0I3xifL1BolMU9UropYVPAqaZ7w/GpwVqCGyFw==",
+    #     "currency": "EUR",
+    #     "passengers": [
+    #         {
+    #             "lastName": "tomson",
+    #             "title": "Mr",
+    #             "firstName": "isabel",
+    #             "email": "asd@asd.com",
+    #             "documentID": "4888789",
+    #             "birthday": "1920-12-30"
+    #         }
+    #     ]
+    # }
+
+    booking_url = "http://128.199.48.38:8080/booking"
+
+    #json_data = urllib.parse.urlencode(json_data)
+    #json_data = json_data.encode('utf-8')
+
+    #req = urllib.request.Request(booking_url, json_data, headers={'content-type': 'application/json'})
+    # r = urllib.request.urlopen(booking_url, json_data)
+    try:
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        r = requests.post(booking_url, json_data,headers=headers)
+        #print(r.status_code)
+        print(r.json()['pnr']) # TADAAA
+        #print(r['pnr'])
+
+        # data = bytes(urllib.parse.urlencode(json_data).encode())
+        # handler = urllib.request.urlopen(booking_url, data)
+        # print(handler.read().decode('utf-8'))
+
+        # with urllib.request.urlopen(req) as response:
+        #     response.read()
+            #print(the_page.decode('utf-8'))
+    except urllib.error.URLError as e:
+        print(e.reason)
+
+    #     the_page = response.read()
+    # print(the_page)
+    # x = r.read().decode('utf-8')
+    # print(x)
+    # print(x['pnr']) # THIS IS WHAT I AM LOOKING FOR
 
     # ### Control print
     # print("Departure airport ITA code: {}".format(airport_name_departure))
@@ -125,45 +170,32 @@ def main(argv):
     # print("Option: {}".format(option))
 
 
-
-def URL_builder(airportDeparture, airportArrival, dateFrom, *args, **kwargs):
+def url_builder(airport_departure, airport_arrival, date_from, **kwargs):
 
     # temp Date Start
-    tDS = str(dateFrom.strftime("%d/%m/%Y"))
+    temp_date_start = str(date_from.strftime("%d/%m/%Y"))
 
-    l = []
+    l = ["https://api.skypicker.com/flights?", "flyFrom=", airport_departure, "&", "to=", airport_arrival, "&",
+         "dateFrom=", temp_date_start, "&", "dateTo=", temp_date_start]
 
-    l.append("https://api.skypicker.com/flights?")
-    l.append("flyFrom=")
-    l.append(airportDeparture)
-    l.append("&")
-    l.append("to=")
-    l.append(airportArrival)
-    l.append("&")
-    l.append("dateFrom=")
-    l.append(tDS)
-    l.append("&")
-    l.append("dateTo=")
-    l.append(tDS)
-
-    if ('dateTo' in kwargs):
+    if 'dateTo' in kwargs:
         # temp Date End
-        tDE = str(kwargs['dateTo'].strftime("%d/%m/%Y"))
+        temp_date_end = str(kwargs['dateTo'].strftime("%d/%m/%Y"))
 
         l.append("&")
         l.append("returnFrom=")
-        l.append(tDE)
+        l.append(temp_date_end)
         l.append("&")
         l.append("returnTo=")
-        l.append(tDE)
+        l.append(temp_date_end)
 
-    if ('option' in kwargs):
+    if 'option' in kwargs:
         l.append("&")
         l.append("sort=")
         l.append(kwargs['option'])
 
-    #l.append("&")
-    #l.append("limit=1")
+    # l.append("&")
+    # l.append("limit=1")
 
     l.append("&")
     l.append("partner=picky")
@@ -171,36 +203,21 @@ def URL_builder(airportDeparture, airportArrival, dateFrom, *args, **kwargs):
     return ''.join(l)
 
 
-
-def return_url_json(URL):
+def return_url_content(url):
     """
     Simple function for retrieving JSON object of requested URL
-    :param URL: 
+    :param url: 
     :return JSON object:
     """
     try:
-        return json.loads(urllib.request.urlopen(URL).read())
+        # params = urllib.parse.urlencode({'spam': 1, 'eggs': 2, 'bacon': 0})
+        # url = "http://www.musi-cal.com/cgi-bin/query?%s" % params
+        return urllib.request.urlopen(url).read().decode('utf-8')
     except:
         print('Exception occurred while handling URL request:')
-        print('-' *100)
+        print('-' * 100)
         raise
 
-
-
-def URL_loader(url):
-    """
-    Request data from URL and process retrieved data.
-    :param url_list: 
-    :return new_url_list: 
-    """
-
-    request_data = return_url_json(url)
-    # print(request_data)
-
-    if int(request_data['_results']) != 0:
-        price = request_data['data'][0]['price']
-        print(price)
-        return request_data['data'][0]['booking_token']
 
 
 # Python 3
